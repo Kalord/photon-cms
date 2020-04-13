4c266c74dc70b14ab424e46984f78e37877d9013
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-04-13 16:10:13 +0300

    Added migration for rule table

---
+++ database/migrations/2020_04_13_121800_create_rule_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Rule;
+
+class CreateRuleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rule', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rule');
+    }
+}
