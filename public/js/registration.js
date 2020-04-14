e3c38451ef033cc0caa95eec106f1c5d54606215
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-04-14 23:07:39 +0800

    Added registration.js

---
+++ public/js/registration.js
@@ -0,0 +1,20 @@
+$('.registration-form').submit((event) => {
+    event.preventDefault();
+
+    const success = () => {
+        $('#name').html($('[name="name"]').val());
+        $('.error-message').hide();
+        $('.error-message').show();
+
+        setTimeout(() => {
+            window.location.href = '/login';
+        }, 300);
+    };
+
+    const error = (html) => {
+        $('.error-message').show();
+        $('.error-message').html(getFirstProperty(html.responseJSON.errors)[0]);
+    }
+
+    handler(event.target, success, error);
+});
