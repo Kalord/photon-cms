65923ed70c266c1830a66fd5499ce09d1e1c6497
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-04-15 04:19:41 +0800

    Added editor.js

---
+++ public/js/editor.js
@@ -0,0 +1,30 @@
+const getEditorData = () => {
+    return {
+        type: 'POST',
+        url: '/admin/post/create',
+        title: $('.title').val(),
+        description: $('.description').val(),
+        keywords: $('.keywords').val(),
+        id_category: $('.categories option:selected').val(),
+        main_img: $('.main-img').prop('files')[0],
+        alt_main_img: $('.alt-main-img').val(),
+        content: $('.note-editable').html(),
+        _token: $('[name="_token"]').val()
+    }
+}
+
+$('.btn-save').click((event) => {
+    let data = getEditorData();
+    data.status = $(event.target).attr('data-status');
+
+    const success = (html) => {
+        console.log(html);
+    };
+
+    const error = (html) => {
+        $('.error-message').show();
+        $('.error-message').html(getFirstProperty(html.responseJSON.errors)[0]);
+    };
+
+    handler(data, success, error);
+});
