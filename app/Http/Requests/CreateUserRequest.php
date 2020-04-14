38500eb174eab2cb356a8f93366a065a1db77dd9
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-04-14 23:08:54 +0800

    Added logic for validation input data

--- app/Http/Requests/CreateUserRequest.php
+++ app/Http/Requests/CreateUserRequest.php
@@ -6,14 +6,29 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class CreateUserRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
+    public function messages()
     {
-        return false;
+        return [
+            'name.required' => 'Необходимо указать имя',
+            'name.min' => 'Минимальный размер имени 3 символова',
+            'name.max' => 'Максимальный размер имени 3 символова',
+            'name.regex' => 'Можно использовать только буквы и цифры',
+
+            'login.required' => 'Необходимо указать логин',
+            'login.min' => 'Минимальный размер имени 3 символова',
+            'login.max' => 'Максимальный размер имени 3 символова',
+            'login.regex' => 'Можно использовать только буквы и цифры',
+            'login.unique'  => 'Логин уже занят',
+
+            'email.required'  => 'Необходимо указать почту',
+            'email.email'  => 'Некорректная почта',
+            'email.unique'  => 'Почта уже занята',
+
+            'password.required'  => 'Необходимо указать пароль',
+            'password.confirmed'  => 'Пароли несовпадают',
+            'password.min'  => 'Минимальный размер пароля 8 символова',
+            'password.max'  => 'Максимальный размер пароля 255 символова'
+        ];
     }
 
     /**
@@ -24,7 +39,10 @@ class CreateUserRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name'      => 'required|min:3|max:30|regex:~\w+~',
+            'login'     => 'required|min:3|max:30|regex:~\w+~|unique:user',
+            'email'     => 'required|email|unique:user',
+            'password'  => 'required|confirmed|min:8|max:255'
         ];
-    }
+    }    
 }
