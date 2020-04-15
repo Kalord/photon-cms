6207baa41b989672b2bd00d7148c8db367007dd8
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-04-16 00:58:42 +0800

    Added update view

---
+++ resources/views/post/update.blade.php
@@ -0,0 +1,144 @@
+@extends('layouts.admin')
+
+@section('title', 'Изменение публикации')
+
+@section('specific_css')
+<link rel="stylesheet" href="/assets/vendor/bootstrap-fileupload/bootstrap-fileupload.min.css" />
+<link rel="stylesheet" href="/assets/vendor/jquery-ui/css/ui-lightness/jquery-ui-1.10.4.custom.css" />
+<link rel="stylesheet" href="/assets/vendor/select2/select2.css" />
+<link rel="stylesheet" href="/assets/vendor/bootstrap-multiselect/bootstrap-multiselect.css" />
+<link rel="stylesheet" href="/assets/vendor/bootstrap-tagsinput/bootstrap-tagsinput.css" />
+<link rel="stylesheet" href="/assets/vendor/bootstrap-colorpicker/css/bootstrap-colorpicker.css" />
+<link rel="stylesheet" href="/assets/vendor/bootstrap-timepicker/css/bootstrap-timepicker.css" />
+<link rel="stylesheet" href="/assets/vendor/dropzone/css/basic.css" />
+<link rel="stylesheet" href="/assets/vendor/dropzone/css/dropzone.css" />
+<link rel="stylesheet" href="/assets/vendor/bootstrap-markdown/css/bootstrap-markdown.min.css" />
+<link rel="stylesheet" href="/assets/vendor/summernote/summernote.css" />
+<link rel="stylesheet" href="/assets/vendor/summernote/summernote-bs3.css" />
+<link rel="stylesheet" href="/assets/vendor/codemirror/lib/codemirror.css" />
+<link rel="stylesheet" href="/assets/vendor/codemirror/theme/monokai.css" />
+@endsection
+
+@section('content')
+<section class="panel">
+    <header class="panel-heading">
+        <h2 class="panel-title">Публикация</h2>
+    </header>
+    <div class="panel-body">
+        <form class="form-horizontal form-bordered" method="get">
+            <div class="form-group">
+                <label class="col-md-3 control-label" for="inputDefault">Заголовок</label>
+                <div class="col-md-6">
+                    <input type="text" class="form-control input-title" id="inputDefault" value="{{$post->title}}">
+                </div>
+            </div>
+            <div class="form-group">
+				<label class="col-md-3 control-label" for="textareaDefault">Описание</label>
+				<div class="col-md-6">
+					<textarea class="form-control description" rows="3" data-plugin-maxlength maxlength="160">{{$post->description}}</textarea>
+					<p>
+						<code>max-length</code> set to 160.
+					</p>
+                </div>
+			</div>
+            <div class="form-group">
+                <label class="col-md-3 control-label" for="inputDefault">Leywords</label>
+                <div class="col-md-6">
+                    <input type="text" class="form-control keywords" id="inputDefault" value="{{$post->keywords}}">
+                </div>
+            </div>
+            <div class="form-group">
+			<label class="col-md-3 control-label">Категория</label>
+			<div class="col-md-6">
+				<select class="form-control categories" data-plugin-multiselect id="ms_example1">
+                    @foreach($categories as $category)
+                        <option value="{{$category->id}}" @php if($post->id_category == $category->id) echo 'selected' @endphp>{{$category->title}}</option>
+                    @endforeach
+				</select>
+			</div>
+            </div>
+            <div class="form-group">
+                <label class="col-md-3 control-label">Обложка</label>
+                <div class="col-md-6">
+                    <div class="fileupload fileupload-new" data-provides="fileupload">
+                        <div class="input-append">
+                            <div class="uneditable-input">
+                                <i class="fa fa-file fileupload-exists"></i>
+                                <span class="fileupload-preview"></span>
+                            </div>
+                            <span class="btn btn-default btn-file">
+                                <span class="fileupload-exists">Change</span>
+                                <span class="fileupload-new">Select file</span>
+                                <input type="file" class="main-img">
+                            </span>
+                            <a href="#" class="btn btn-default fileupload-exists" data-dismiss="fileupload">Remove</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-md-3 control-label" for="inputDefault">Описание обложки</label>
+                <div class="col-md-6">
+                    <input type="text" class="form-control alt-main-img" id="inputDefault" value="{{$post->alt_main_img}}">
+                </div>
+            </div>
+        </form>
+    </div>
+</section>
+
+
+<div class="row">
+    <div class="col-xs-12">
+        <section class="panel">
+            <header class="panel-heading">
+                <h2 class="panel-title">Редактор</h2>
+            </header>
+            <div class="panel-body">
+                <form class="form-horizontal form-bordered">
+                    <div class="form-group">
+                        <label class="col-md-3 control-label">Summernote</label>
+                        <div class="col-md-9">
+                            <div class="summernote" data-plugin-summernote data-plugin-options='{ "height": 180, "codemirror": { "theme": "ambiance" } }'>{{$post->content}}</div
+                        </div>
+                    </div>
+                </div>
+            </form>
+            </div>
+        </section>
+    </div>
+</div>
+
+{{@csrf_field()}}
+
+<div class="alert alert-danger error-message" style="display: none;"></div>
+<a class="btn btn-primary btn-update">Изменить</a>
+@endsection
+
+
+@section('specific_js')
+    <script src="/assets/vendor/bootstrap-fileupload/bootstrap-fileupload.min.js"></script>
+    <script src="/assets/vendor/jquery-ui/js/jquery-ui-1.10.4.custom.js"></script>
+    <script src="/assets/vendor/jquery-ui-touch-punch/jquery.ui.touch-punch.js"></script>
+    <script src="/assets/vendor/select2/select2.js"></script>
+    <script src="/assets/vendor/bootstrap-multiselect/bootstrap-multiselect.js"></script>
+    <script src="/assets/vendor/jquery-maskedinput/jquery.maskedinput.js"></script>
+    <script src="/assets/vendor/bootstrap-tagsinput/bootstrap-tagsinput.js"></script>
+    <script src="/assets/vendor/bootstrap-colorpicker/js/bootstrap-colorpicker.js"></script>
+    <script src="/assets/vendor/bootstrap-timepicker/js/bootstrap-timepicker.js"></script>
+    <script src="/assets/vendor/fuelux/js/spinner.js"></script>
+    <script src="/assets/vendor/dropzone/dropzone.js"></script>
+    <script src="/assets/vendor/bootstrap-markdown/js/markdown.js"></script>
+    <script src="/assets/vendor/bootstrap-markdown/js/to-markdown.js"></script>
+    <script src="/assets/vendor/bootstrap-markdown/js/bootstrap-markdown.js"></script>
+    <script src="/assets/vendor/codemirror/lib/codemirror.js"></script>
+    <script src="/assets/vendor/codemirror/addon/selection/active-line.js"></script>
+    <script src="/assets/vendor/codemirror/addon/edit/matchbrackets.js"></script>
+    <script src="/assets/vendor/codemirror/mode/javascript/javascript.js"></script>
+    <script src="/assets/vendor/codemirror/mode/xml/xml.js"></script>
+    <script src="/assets/vendor/codemirror/mode/htmlmixed/htmlmixed.js"></script>
+    <script src="/assets/vendor/codemirror/mode/css/css.js"></script>
+    <script src="/assets/vendor/summernote/summernote.js"></script>
+    <script src="/assets/vendor/bootstrap-maxlength/bootstrap-maxlength.js"></script>
+    <script src="/assets/vendor/ios7-switch/ios7-switch.js"></script>
+    <script src="/assets/vendor/bootstrap-confirmation/bootstrap-confirmation.js"></script>
+@endsection
