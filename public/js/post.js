7832057aa7972fe3c88e4cf93c856b90776f566a
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-04-16 00:59:43 +0800

    Added docs

--- public/js/post.js
+++ public/js/post.js
@@ -1,3 +1,7 @@
+/**
+ * Поисковые параметры
+ * @return object
+ **/
 const getFindOptions = () => {
     let findOptions = {};
     let pivot = $('.admin-post-list tr').last().attr('data-id');
@@ -9,20 +13,34 @@ const getFindOptions = () => {
     return findOptions;
 }
 
+/**
+ * Заголовки статусов
+ * @var object
+ **/
 const statusTitle = [
     'Удален',
     'Черновик',
     'Активен'
 ];
 
+/**
+ * Получение загловка статуса по идентификатору
+ * @param {int} status
+ **/
 const getStatusTitle = (status) => {
     return statusTitle[status];
 }
 
+/**
+ * Получение кнопок действий в зависимости от статуса публикации
+ *
+ * @param {int} status
+ *
+ * @returns string
+ **/
 const getActionButton = (status) => {
     if (status == 0) {
         return `
-            <a class="btn btn-primary">Редактировать</a>
             <a class="btn btn-primary" data-action="to-active" style="margin-top: 5px;">Восстановить</a>
             <a class="btn btn-danger" data-action="delete" style="margin-top: 5px;">Удалить безвозратно</a>
         `
@@ -30,7 +48,6 @@ const getActionButton = (status) => {
 
     if (status == 1) {
         return `
-            <a class="btn btn-primary">Редактировать</a>
             <a class="btn btn-primary btn-action" data-action="to-active" style="margin-top: 5px;">Сделать активной</a>
             <a class="btn btn-danger btn-action" data-action="to-trash" style="margin-top: 5px;">Удалить</a>
         `
@@ -38,13 +55,20 @@ const getActionButton = (status) => {
 
     if (status == 2) {
         return `
-            <a class="btn btn-primary">Редактировать</a>
             <a class="btn btn-primary btn-action" data-action="to-draft" style="margin-top: 5px;">В черновик</a>
             <a class="btn btn-danger btn-action" data-action="to-trash"  style="margin-top: 5px;">Удалить</a>
         `
     }
 }
 
+/**
+ * Изменение статуса публикации
+ *
+ * @param {int} id_post
+ * @param {string} url
+ *
+ * @return string
+ **/
 const changeStatus = (id_post, url) => {
     let newAction;
 
@@ -61,9 +85,14 @@ const changeStatus = (id_post, url) => {
         }
     })
 
-    return newAction;
+    return '<a href="#" class="btn btn-primary">Редактировать</a>' + newAction;
 };
 
+/**
+ * Получение URL обработчиков действий
+ *
+ * @return string
+ **/
 const getActionHandler = (action) => {
     const actionHandler = {
         'to-active': '/admin/post/action/to-active',
@@ -73,6 +102,16 @@ const getActionHandler = (action) => {
     }
 
     return actionHandler[action];
+}
+
+const btnActionHandler = (event) => {
+    let action = $(event.target).attr('data-action');
+    let id = $(event.target).parent().parent().attr('data-id');
+
+    let newAction = changeStatus(id, getActionHandler(action));
+
+    $(event.target).parent().html(newAction);
+    $('.btn-action').click(btnActionHandler);
 }
 
 let showPosts = () => {
@@ -95,20 +134,14 @@ let showPosts = () => {
                         <td>${getStatusTitle(post.status)}</td>
                         <td>${post.view}</td>
                         <td>
+                            <a href="/admin/post/update/${post.id}" class="btn btn-primary">Редактировать</a>
                             ${action}
                         </td>
                     </tr>
                 `);
             });
 
-            $('.btn-action').click((event) => {
-                let action = $(event.target).attr('data-action');
-                let id = $(event.target).parent().parent().attr('data-id');
-
-                let newAction = changeStatus(id, getActionHandler(action));
-
-                $(event.target).parent().html(newAction);
-            });
+            $('.btn-action').click(btnActionHandler);
         }
     })
 }
