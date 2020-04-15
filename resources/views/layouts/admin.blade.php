41453fe03c80be6202eeaf514dea9e4c69c46bb6
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-04-15 15:29:30 +0800

    Update

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,8 +10,6 @@
     <!-- Mobile Metas -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
-
     <!-- Web Fonts  -->
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800|Shadows+Into+Light" rel="stylesheet" type="text/css">
 
